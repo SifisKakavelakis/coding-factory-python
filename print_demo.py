34e0f4aba66b524bcfc968b9390cde34191eddad
Iosif Kakavelakis <sifiskakavelakis2004@gmail.com>
2026-06-03 20:59:57 +0300

    feat: add print_demo.py

---
+++ print_demo.py
@@ -0,0 +1,13 @@
+num = 16
+height = 1.87
+is_raining = False
+message = "I love Python dev"
+
+print(num)
+print(height)
+print(is_raining)
+print(message)
+
+print(num, height, is_raining, message, sep="-")
+
+print()
