d410901bf1bea3468ab131e70fa691d65c97ee7a
Iosif Kakavelakis <sifiskakavelakis2004@gmail.com>
2026-06-04 20:43:39 +0300

    feat: add input_demo_v2

---
+++ chapter01/06_input_demo.py
@@ -0,0 +1,14 @@
+# name = input("Please enter your name: ")
+# age = int(input("Please enter your age: "))
+# height = float(input("Please enter your height: "))
+is_student = input("Are you a student? (yes/no)").loweri == "yes"
+
+if is_student:
+    print("You are student")
+else:
+    print("You are teacher")
+
+# if is_student == "yes":
+#     print("You are student")
+# else:
+#     print("You are teacher")
