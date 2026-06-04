f44dc2f10da765eab4a77b56938453f8fe6901f2
Iosif Kakavelakis <sifiskakavelakis2004@gmail.com>
2026-06-04 20:42:54 +0300

    feat: add print_demo

---
+++ chapter01/03_print_demo.py
@@ -0,0 +1,17 @@
+my_int = 10
+my_float = 5.7
+my_str = "Hello CF9"
+
+print("hello comma seperated values: ")
+
+print(my_int, my_float, my_str)
+
+print("-------------------------")
+
+print(my_int, my_float, my_str, sep='\t')
+
+print("-------------------------")
+
+print(my_int, my_float, my_str, sep='\t', end='*')
+
+print("Hello friends")
