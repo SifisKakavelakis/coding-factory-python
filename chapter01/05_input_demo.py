6090daa82b39739a1845e80bb4b934822b276cc5
Iosif Kakavelakis <sifiskakavelakis2004@gmail.com>
2026-06-04 20:43:19 +0300

    feat: add input_demo

---
+++ chapter01/05_input_demo.py
@@ -0,0 +1,9 @@
+name = input("Please enter your name: ")
+print("Hello", name)
+
+year_of_birth = int(input("Please enter the year of your birth: "))
+# year_of_birth = int(year_of_birth)
+print("You are", 2026 - year_of_birth, "years old.")
+
+height = float(input("Please enter your height in cm: "))
+print("You are", height / 100, "meters tall.")
