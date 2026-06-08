897e1b897e136ddcfd5d94d5950a311e91b2b4f0
Iosif Kakavelakis <sifiskakavelakis2004@gmail.com>
2026-06-08 18:17:28 +0300

    feat: add string slicing

---
+++ chapter01/21_string_slicing.py
@@ -0,0 +1,26 @@
+s = "Coding Factory"
+
+s1 = s[7]
+
+s2 = s[:6]
+
+s3 = s[7:] 
+
+s4 = s[:]  
+
+s5 = s[:-1]  
+
+s6 = s[:-2]  
+
+print(s1)  
+print(s2)  
+print(s3)  
+print(s4)  
+print(s5)  
+print(s6)  
+
+r_s = s[::-1]  
+print(r_s) 
+
+s_q = s[7:7]  
+print(s_q) 
